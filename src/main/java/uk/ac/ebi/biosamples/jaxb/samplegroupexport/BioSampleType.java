6278716b979a0d05620b76d6af7c299ba1853a7d
Adam Faulconbridge <afaulconbridge@googlemail.com>
2015-11-02 16:33:07 +0000

    Enabled top level elements of BioSample

--- src/main/java/uk/ac/ebi/biosamples/jaxb/samplegroupexport/BioSampleType.java
+++ src/main/java/uk/ac/ebi/biosamples/jaxb/samplegroupexport/BioSampleType.java
@@ -14,6 +14,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 /**
@@ -45,6 +46,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "BioSample")
 @XmlType(name = "bioSampleType", propOrder = { "annotation", "property", "derivedFrom", "database", "groupIds" })
 public class BioSampleType {
 
